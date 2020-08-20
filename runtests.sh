e9384d429014c4dcb22c0cec632f6dadfd41eb8f
Stanislav Miroshnichenko <celt@root.in.ua>
2020-08-20 13:35:43 +0300

    Back to AWS step

--- runtests.sh
+++ runtests.sh
@@ -5,3 +5,4 @@ _project="${1:-project}"
 
 echo "====running tests for $_project in $_environment===="
 mvn test -pl $_project -Dkarate.env=$_environment
+aws s3 cp ./target/cucumber-html-reports s3://folio-gulfstream.s3.amazonaws.com/
