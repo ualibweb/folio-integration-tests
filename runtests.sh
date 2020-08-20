f28c081c93f5010fc81f53019a90d9939aace068
Stanislav Miroshnichenko <celt@root.in.ua>
2020-08-20 13:36:37 +0300

    sh script changed

--- runtests.sh
+++ runtests.sh
@@ -5,4 +5,3 @@ _project="${1:-project}"
 
 echo "====running tests for $_project in $_environment===="
 mvn test -pl $_project -Dkarate.env=$_environment
-aws s3 cp ./target/cucumber-html-reports s3://folio-gulfstream.s3.amazonaws.com/
